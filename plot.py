a9459a39ff637adcd3e2713c7e007762f40ab1fa
zcajiayin <cong.zhang92@gmail.com>
2021-06-22 18:42:03 +0800

    general-update

--- plot.py
+++ plot.py
@@ -10,7 +10,7 @@ log = np.load('log/log1.npy')
 
 obj = log[:horizon, 0].reshape(log.shape[0] // plot_step_size, -1).mean(axis=1)
 # plot objective...
-plt.xlabel('iteration')
+plt.xlabel('iteration({})'.format(plot_step_size))
 plt.ylabel('make span')
 plt.plot([_ for _ in range(obj.shape[0])], obj, color='tab:blue')
 plt.grid()
@@ -22,7 +22,7 @@ plt.close()
 
 returns = log[:horizon, 1].reshape(log.shape[0] // plot_step_size, -1).mean(axis=1)
 # plot objective...
-plt.xlabel('iteration')
+plt.xlabel('iteration({})'.format(plot_step_size))
 plt.ylabel('return')
 plt.plot([_ for _ in range(returns.shape[0])], returns, color='tab:blue')
 plt.grid()
@@ -34,7 +34,7 @@ plt.close()
 
 running_returns = log[:horizon, 2].reshape(log.shape[0] // plot_step_size, -1).mean(axis=1)
 # plot objective...
-plt.xlabel('iteration')
+plt.xlabel('iteration({})'.format(plot_step_size))
 plt.ylabel('running return')
 plt.plot([_ for _ in range(running_returns.shape[0])], running_returns, color='tab:blue')
 plt.grid()

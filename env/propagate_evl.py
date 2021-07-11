078eccd21281c33bb65b00acfe2ef0c929b3a8eb
zcajiayin <cong.zhang92@gmail.com>
2021-07-11 12:15:12 +0800

    backward-eval-v1

--- env/propagate_evl.py
+++ env/propagate_evl.py
@@ -38,8 +38,10 @@ class BackwardPass(MessagePassing):
         """"""
         if isinstance(x, Tensor):
             x: OptPairTensor = (x, x)
+        print(x)
         # propagate_type: (x: OptPairTensor)
         out = self.propagate(edge_index, x=x, size=size)
+        print(out)
         return out
 
 
@@ -61,7 +63,7 @@ if __name__ == "__main__":
     state, feasible_action, done = env.reset(instance=inst, fix_instance=True)
     t2 = time.time()
 
-    # testing forward pass
+    '''# testing forward pass
     dur_earliest_st = torch.from_numpy(np.pad(inst[0].reshape(-1), (1, 1), 'constant', constant_values=0)).reshape(-1, 1)
     forward_pass = ForwardPass(aggr='max', flow="source_to_target")
     earliest_st = torch.zeros(size=[j * m + 2, 1], dtype=torch.float32)
@@ -79,31 +81,39 @@ if __name__ == "__main__":
         ma_earliest_st = forward_pass(x=ma_earliest_st, edge_index=adj_earliest_st)
     t4 = time.time()
     if torch.equal(earliest_st.squeeze() / 1000, state.x[:, 1]):
-        print('forward pass is OK! It takes:', t4 - t3, 'networkx version forward pass and backward pass take:', t2 - t1)
+        print('forward pass is OK! It takes:', t4 - t3, 'networkx version forward pass and backward pass take:', t2 - t1)'''
 
     print()
 
     # testing backward pass
     dur_latest_st = torch.from_numpy(np.pad(inst[0].reshape(-1), (1, 1), 'constant', constant_values=0)).reshape(-1, 1)
+    print(dur_latest_st)
     backward_pass = BackwardPass(aggr='max', flow="source_to_target")
     latest_st = torch.zeros(size=[j * m + 2, 1], dtype=torch.float32)
-    latest_st[-1] = float(state.y)
+    latest_st[-1] = - float(state.y)
     print(latest_st)
     adj_latest_st = torch.flip(state.edge_index[:, state.edge_index[0] != state.edge_index[1]], dims=[0, 1])
     print(adj_latest_st)
     ma_latest_st = torch.ones(size=[j * m + 2, 1], dtype=torch.int8)
     ma_latest_st[-1] = 0
+    print()
     t3 = time.time()
-    for _ in range(j * m + 2):  #
+    for _ in range(1):  # j * m + 2
         if ma_latest_st.sum() == 0:
             print('finish backward pass at step:', _)
             break
-        x = latest_st.masked_fill(ma_latest_st, 0) - dur_latest_st
-        print(x)
+        # print(ma_latest_st)
+        # x = dur_latest_st + latest_st.masked_fill(ma_latest_st, 0)
+        x = dur_latest_st + latest_st
+        # print(latest_st.masked_fill(ma_latest_st, 0))
+        # print(x)
         latest_st = backward_pass(x=x, edge_index=adj_latest_st)
-        print(latest_st)
+        # print(ma_latest_st)
+        # print(latest_st)
+        # print(latest_st)
         ma_latest_st = backward_pass(x=ma_latest_st, edge_index=adj_latest_st)
+        # print()
     t4 = time.time()
-    print(latest_st)
+    # print(latest_st)
     if torch.equal(latest_st.squeeze() / 1000, state.x[:, 2]):
         print('backward pass is OK! It takes:', t4 - t3, 'networkx version forward pass and backward pass take:', t2 - t1)

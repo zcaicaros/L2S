73b4b353c2c0071edb475027524d41317da9dbd6
zcajiayin <cong.zhang92@gmail.com>
2021-07-12 21:25:27 +0800

    general-update

--- env/env_batch.py
+++ env/env_batch.py
@@ -366,16 +366,12 @@ class JsspN5:
         np.fill_diagonal(adj, 1)
         topological_order = list(nx.topological_sort(G))
         est_ST = np.fromiter(jsp.forward_pass(graph=G, topological_order=topological_order).values(), dtype=np.float32)
-        lst_ST = np.fromiter(
-            jsp.backward_pass(graph=G, topological_order=topological_order, makespan=est_ST[-1]).values(),
-            dtype=np.float32)
+        lst_ST = np.fromiter(jsp.backward_pass(graph=G, topological_order=topological_order, makespan=est_ST[-1]).values(), dtype=np.float32)
         f1 = torch.from_numpy(np.pad(np.float32((instance[0].reshape(-1, 1)) / self.high), ((1, 1), (0, 0)), 'constant', constant_values=0))
         f2 = torch.from_numpy(est_ST.reshape(-1, 1) / self.fea_norm_const)
         f3 = torch.from_numpy(lst_ST.reshape(-1, 1) / self.fea_norm_const)
         x = torch.cat([f1, f2, f3], dim=-1)
         edge_idx = torch.nonzero(torch.from_numpy(adj)).t().contiguous()
-        # print(adj)
-        # print(edge_idx)
         return Data(x=x, edge_index=edge_idx, y=np.amax(est_ST))
 
     def _transit_single(self, plot, args):

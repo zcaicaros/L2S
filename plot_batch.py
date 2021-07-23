41134c5c0206b83714a1b149aef3b854da572a56
zcajiayin <cong.zhang92@gmail.com>
2021-07-24 07:50:22 +0800

    add-new-log-and-saved-models

--- plot_batch.py
+++ plot_batch.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 show = True
 j = 10
 m = 10
-episode = 64000
+episode = 128000
 transit = 128
 init = 'fdd-divide-mwkr'  # 'plist', 'spt', ...
-file = './log/batch_log_{}x{}_{}w_{}_{}.npy'.format(j, m, str(episode/10000), init, transit)
-plot_step_size = 10
+file = './log/batch_validation_log_{}x{}_{}w_{}_{}.npy'.format(j, m, str(episode/10000), init, transit)
+plot_step_size = 1
 log = np.load(file)
 
-obj = log[:log.shape[0]//plot_step_size*plot_step_size].reshape(log.shape[0] // plot_step_size, -1).mean(axis=1)
+obj = log[:log.shape[0]//plot_step_size*plot_step_size, 1].reshape(log.shape[0] // plot_step_size, -1).mean(axis=1)
 # plot objective...
 plt.xlabel('iteration({})'.format(plot_step_size))
 plt.ylabel('make span')

f46efd758ea668dd7fa7f5b9e844c853530f1494
zcaicaros <cong.zhang92@gmail.com>
2021-07-30 09:21:44 +0800

    general-update

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@ import argparse
 parser = argparse.ArgumentParser(description='DRL-LSJSP')
 
 # env parameters
-parser.add_argument('--j', type=int, default=20)
+parser.add_argument('--j', type=int, default=10)
 parser.add_argument('--m', type=int, default=10)
 parser.add_argument('--l', type=int, default=1)
 parser.add_argument('--h', type=int, default=99)
@@ -13,7 +13,7 @@ parser.add_argument('--hidden_dim', type=int, default=128)
 parser.add_argument('--embedding_layer', type=int, default=4)
 parser.add_argument('--policy_layer', type=int, default=4)
 # training parameters
-parser.add_argument('--lr', type=float, default=1e-5)
+parser.add_argument('--lr', type=float, default=1e-4)
 parser.add_argument('--gamma', type=float, default=1)
 parser.add_argument('--steps_learn', type=int, default=10)
 parser.add_argument('--transit', type=int, default=500)

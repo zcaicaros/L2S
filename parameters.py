46b363196881d4e0c9f9011a36f7a4423007bd7a
zcaicaros <cong.zhang92@gmail.com>
2021-08-12 08:24:54 +0800

    general_update

--- parameters.py
+++ parameters.py
@@ -18,9 +18,9 @@ parser.add_argument('--heads', type=int, default=1)  # dghan parameters
 parser.add_argument('--drop_out', type=float, default=0)  # dghan parameters
 # training parameters
 parser.add_argument('--lr', type=float, default=5e-5)
-parser.add_argument('--steps_learn', type=int, default=10)
+parser.add_argument('--steps_learn', type=int, default=5)
 parser.add_argument('--transit', type=int, default=500)
-parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--episodes', type=int, default=128000)
 parser.add_argument('--step_validation', type=int, default=10)
 

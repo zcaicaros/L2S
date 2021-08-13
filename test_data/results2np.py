69261aeb0d9d9d33f427cbea1e89aa30d2697655
zcajiayin <cong.zhang92@gmail.com>
2021-08-13 14:57:00 +0800

    general_update

--- test_data/results2np.py
+++ test_data/results2np.py
@@ -3,6 +3,6 @@ import numpy as np
 problem = 'yn'
 j = 20
 m = 20
-results = np.array([884, 904, 892, 968], dtype=float)
-print(results)
-np.save('{}{}x{}_result.npy'.format(problem, j, m), results)
+upper_bounds = np.array([884, 904, 892, 968], dtype=float)
+print(upper_bounds)
+np.save('{}{}x{}_result.npy'.format(problem, j, m), upper_bounds)

ab0e955c65581435ef1ee637380721a4b3e1abce
zcajiayin <cong.zhang92@gmail.com>
2021-08-25 18:39:24 +0800

    update_plt_code

--- curves/plot_gin_dghan_gin+dghan.py
+++ curves/plot_gin_dghan_gin+dghan.py
@@ -33,10 +33,10 @@ step_validation = 10
 total_plt_steps = 200
 show = True
 save = False
-log_type = 'validation'  # 'training', 'validation'
+log_type = 'training'  # 'training', 'validation'
 plot_step_size_training = (episodes // batch_size) // total_plt_steps
 plot_step_size_validation = (episodes // batch_size) // (total_plt_steps * 5)
-save_file_type = '.png'
+save_file_type = '.pdf'
 
 
 if embedding_type1 == 'gin':
@@ -112,7 +112,8 @@ if log_type == 'training':
     plt.legend()
     if save:
         plt.savefig('./{}{}'.format('merged_training_log', save_file_type))
-    plt.show()
+    if show:
+        plt.show()
 
 else:
     obj_incumbent1 = log1[:log1.shape[0]//plot_step_size_validation*plot_step_size_validation, 0].reshape(log1.shape[0] // plot_step_size_validation, -1).mean(axis=1)
@@ -130,7 +131,8 @@ else:
     plt.legend()
     if save:
         plt.savefig('./{}{}'.format('merged_incumbent_validation_log', save_file_type))
-    plt.show()
+    if show:
+        plt.show()
 
     obj_last_step1 = log1[:log1.shape[0] // plot_step_size_validation * plot_step_size_validation, 1].reshape(log1.shape[0] // plot_step_size_validation, -1).mean(axis=1)
     obj_last_step2 = log2[:log2.shape[0] // plot_step_size_validation * plot_step_size_validation, 1].reshape(log2.shape[0] // plot_step_size_validation, -1).mean(axis=1)
@@ -147,7 +149,8 @@ else:
     plt.legend()
     if save:
         plt.savefig('./{}{}'.format('merged_last-step_validation_log', save_file_type))
-    plt.show()
+    if show:
+        plt.show()
 
 
 

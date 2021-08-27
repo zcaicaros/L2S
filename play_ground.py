da6600752e6bc073834ec242f1c1930a25df759a
zcaicaros <cong.zhang92@gmail.com>
2021-08-27 08:42:28 +0800

    upload_part_of_conventional_results

--- play_ground.py
+++ play_ground.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 
-print(np.load('./test_data/{}{}x{}.npy'.format('syn', 200, 50)).shape)
+print(np.load('./ortools_result/ortools_{}{}x{}_result.npy'.format('tai', 30, 20)).shape)
+print(np.load('./ortools_result/ortools_{}{}x{}_time.npy'.format('tai', 30, 20)).shape)
 np.save('./test_data/{}{}x{}_time.npy'.format('syn', 100, 20), 3600*np.ones(100).reshape(-1, 1))

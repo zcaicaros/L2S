d1632da7f7f1de23e1c190093a3dc07235df2ebf
zcaicaros <cong.zhang92@gmail.com>
2021-08-30 23:16:36 +0800

    add_chunks_to_test_learned.py

--- play_ground.py
+++ play_ground.py
@@ -7,7 +7,7 @@ import numpy as np
 # print(np.load('./test_results/DRL_results/incumbent_10x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_4e-05_10_500_64_256000_10/{}_{}x{}_{}_result.npy'.format('abz', 10, 10, 'fdd-divide-mwkr')))
 # print(np.load('./test_results/DRL_results/incumbent_10x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_4e-05_10_500_64_256000_10/{}_{}x{}_{}_time.npy'.format('abz', 10, 10, 'fdd-divide-mwkr')))
 
-print(np.load('./test_results/conventional_results/swv20x15_best-improvement-policy_fdd-divide-mwkr_result.npy'))
+# print(np.load('./test_results/conventional_results/swv20x15_best-improvement-policy_fdd-divide-mwkr_result.npy'))
 
 a = [1, 2]
 import random
@@ -21,3 +21,7 @@ print()
 
 arr = np.load('./test_data/{}{}x{}_result.npy'.format('syn', 100, 20))
 print(arr)
+
+arr_sum = np.add([arr, arr, arr])
+print(arr_sum)
+print(arr_sum / 3)
--- test_learned.py
+++ test_learned.py
@@ -41,7 +41,7 @@ def main():
 
     # model config
     model_j = 20  # 10， 15， 15， 20， 20
-    model_m = 10  # 10， 10， 15， 10， 15
+    model_m = 15  # 10， 10， 15， 10， 15
     model_l = 1
     model_h = 99
     model_init_type = 'fdd-divide-mwkr'
@@ -100,7 +100,7 @@ def main():
 
         for p_j, p_m in zip(problem_j, problem_m):  # select problem size
 
-            inst = np.load('./test_data/{}{}x{}.npy'.format(test_t, p_j, p_m))[:20]
+            inst = np.load('./test_data/{}{}x{}.npy'.format(test_t, p_j, p_m))
             print('\nStart testing {}{}x{}...'.format(test_t, p_j, p_m))
 
             # read saved gap_against or use ortools to solve it.
@@ -162,14 +162,18 @@ def main():
                     with open(which_model + '__init__.py', 'w'): pass  # make created folder as python package by creating __init__.py
                 result_file = Path(which_model + which_dateset + '_result.npy')
                 time_file = Path(which_model + which_dateset + '_time.npy')
-                if not result_file.is_file() or not time_file.is_file():
+                # if not result_file.is_file() or not time_file.is_file():
+                if True:
                     print('Starting rollout DRL policy...')
-                    if p_j >= 100 and test_t == 'syn':
-                        chunk_size = 20
+                    if p_j >= 100 and inst.shape[0] >= 20:
+                        chunk_size = 10
+                        print('Problem of size {}x{} containing {} instances is too large to form a batch. Splitting into chunks and test seperately. Chunk size is {}.'.format(p_j, p_m, inst.shape[0], chunk_size))
                     else:
                         chunk_size = inst.shape[0]
-                    chunk_result_each_init, chunk_time_each_init = [], []
-                    for i in range(inst.shape[0] // chunk_size):
+                    n_chunks = inst.shape[0] // chunk_size
+                    results_each_init, inference_time_each_init = [], []
+                    for i in range(n_chunks):
+                        chunk_result, chunk_time = [], []
                         inst_chunk = inst[i*chunk_size:(i+1)*chunk_size]
                         batch_data = BatchGraph()
                         states, feasible_actions, _ = env.reset(instances=inst_chunk, init_type=init, device=dev, plot=show)
@@ -184,15 +188,25 @@ def main():
                                         DRL_result = env.incumbent_objs.cpu().squeeze().numpy()
                                     else:
                                         DRL_result = env.current_objs.cpu().squeeze().numpy()
-                                    chunk_result_each_init.append(DRL_result)
-                                    chunk_time_each_init.append((time.time() - drl_start) / inst.shape[0])
-                    # print('For testing steps: {}    '.format(env.itr),
-                    #       'DRL Gap: {:.6f}    '.format(((DRL_result - gap_against) / gap_against).mean()),
-                    #       'DRL results takes: {:.6f} per instance.'.format((time.time() - drl_start) / inst.shape[0]))
-                    results_each_init = np.stack(results_each_init)
-                    inference_time_each_init = np.array(inference_time_each_init).reshape(-1, 1)
-                    np.save(which_model + which_dateset + '_result.npy', results_each_init)
-                    np.save(which_model + which_dateset + '_time.npy', inference_time_each_init)
+                                    chunk_result.append(DRL_result)
+                                    chunk_time.append((time.time() - drl_start) / inst_chunk.shape[0])
+                                    if n_chunks == 1:
+                                        print('For testing steps: {}    '.format(env.itr),
+                                              'DRL Gap: {:.6f}    '.format(((DRL_result - gap_against) / gap_against).mean()),
+                                              'DRL results takes: {:.6f} per instance.'.format((time.time() - drl_start) / inst.shape[0]))
+                        results_each_init.append(np.stack(chunk_result))
+                        inference_time_each_init.append(np.array(chunk_time))
+                    results_each_init = np.concatenate(results_each_init, axis=-1)
+                    inference_time_each_init = (np.stack(inference_time_each_init).sum(axis=0) / n_chunks) / chunk_size
+                    if n_chunks > 1:
+                        for i, step in enumerate(performance_milestones):
+                            print('For testing steps: {}    '.format(step),
+                                  'DRL Gap: {:.6f}    '.format(((results_each_init[i] - gap_against) / gap_against).mean()),
+                                  'DRL results takes: {:.6f} per instance.'.format(inference_time_each_init[i]))
+                    # np.save(which_model + which_dateset + '_result.npy', results_each_init)
+                    # np.save(which_model + which_dateset + '_time.npy', inference_time_each_init)
+                    print(results_each_init)
+                    print(inference_time_each_init)
                 else:
                     print('Results already exist.')
 

bcfa3946c215b7abfdf9cda8c43d58b8a76b8108
zcaicaros <cong.zhang92@gmail.com>
2021-08-30 09:16:29 +0800

    upload_conven

--- play_ground.py
+++ play_ground.py
@@ -8,3 +8,10 @@ import numpy as np
 # print(np.load('./test_results/DRL_results/incumbent_10x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_4e-05_10_500_64_256000_10/{}_{}x{}_{}_time.npy'.format('abz', 10, 10, 'fdd-divide-mwkr')))
 
 print(np.load('./test_results/conventional_results/swv20x15_best-improvement-policy_fdd-divide-mwkr_result.npy'))
+
+a = [1, 2]
+import random
+a_ele = random.choice(a)
+print(a_ele)
+a_ele = 3
+print(a)

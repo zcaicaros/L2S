63cddce51b5fbf9b4fe037a3c2b8d57deba6258c
zcajiayin <cong.zhang92@gmail.com>
2021-09-03 08:06:04 +0800

    upload_20x15_DRL_results_time

--- play_ground.py
+++ play_ground.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 
-print(np.load('./test_results/DRL_results/incumbent_20x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_5e-05_10_500_64_128000_10/syn_100x20_fdd-divide-mwkr_time.npy'))
+print(np.load('./test_results/DRL_results/incumbent_20x15[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_5e-05_10_500_64_128000_10/syn_100x20_fdd-divide-mwkr_result.npy'))

2ec4c8c9ffaf9713d4e0b4ea9d4466899a460ae7
unknown <cong.zhang92@gmail.com>
2021-11-06 16:20:59 +0800

    general-update

--- play_ground.py
+++ play_ground.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 
-print(np.load('./log/training_log_10x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_5e-05_10_500_64_128000_10.npy')[:40])
+# print(np.load('./log/training_log_10x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_5e-05_10_500_64_128000_10.npy')[:40])
+
+a = np.load('test_results/DRL_results/incumbent_20x10[1,99]_fdd-divide-mwkr_yaoxin_1_128_4_4_gin+dghan_1_0.0_5e-05_10_500_64_128000_10/tai_30x20_fdd-divide-mwkr_result.npy')[-1]
+b = np.load('test_data/tai30x20_result.npy')
+onlineDRL = np.array([2208, 2168, 2086, 2261, 2227, 2349, 2101, 2267, 2154, 2216], dtype=np.float32)
+
+print(((onlineDRL - b)/b).mean())
